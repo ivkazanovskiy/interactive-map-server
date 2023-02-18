4d179ac3d82512f0d2a0100b5839775ed2be13b0
Ivan Kazanovskiy <ivkazanovskiy@gmail.com>
2023-02-18 14:06:52 +0300

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
 import { Config } from './modules/other/config/config.service';
+import { swaggerConfig } from './swager.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,9 @@ async function bootstrap() {
   } = app.get(Config);
 
   const logger = new Logger('bootstrap');
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, host);
   logger.log(`Server is listening on ${host}:${port}`);
---
+++ src/swager.config.ts
@@ -0,0 +1,3 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const swaggerConfig = new DocumentBuilder().setVersion('1.0').build();
